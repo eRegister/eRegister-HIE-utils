9bfcd07c409235fc39f3f066d8e51922ea83ea96
leqela-prog <seeisoleqelat@gmail.com>
2026-01-21 11:33:39 +0200

    updating the new key

--- scripts/install_cert_dhis.sh
+++ scripts/install_cert_dhis.sh
@@ -2,7 +2,9 @@
 
 /development_emr/eRegister-HIE-utils/gitpull_eRegister_HIE_utils.sh
 
-docker exec -it bahmni_docker_emr-service_1 sudo keytool -import -keystore /usr/java/jre1.8.0_131/lib/security/cacerts -storepass changeit -alias livedhis -file /development/eRegister-HIE-utils/fullchain_dhisls.crt -noprompt
+docker exec -it bahmni_docker_emr-service_1 sudo keytool -delete -keystore /usr/java/jre1.8.0_131/lib/security/cacerts -storepass changeit -alias livedhis -noprompt
+
+docker exec -it bahmni_docker_emr-service_1 sudo keytool -import -keystore /usr/java/jre1.8.0_131/lib/security/cacerts -storepass changeit -alias livedhis -file /development/eRegister-HIE-utils/dhisls.crt -noprompt
 
 docker exec bahmni_docker_emr-service_1 systemctl restart openmrs
 
